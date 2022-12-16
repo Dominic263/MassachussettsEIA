8419edfcff1c0e29f0d5b8316a2f2a14b7f2d507
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2022-12-16 10:20:42 -0500

    Initial Commit

---
+++ MassachussettsEIA/MassachussettsEIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MassachussettsEIAApp.swift
+//  MassachussettsEIA
+//
+//  Created by DOMINIC NDONDO on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MassachussettsEIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
